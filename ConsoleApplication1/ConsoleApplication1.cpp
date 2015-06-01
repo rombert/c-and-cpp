6b0c516d15912f13216f3bfc7e9f6f1d5f52a797
Robert Munteanu <robert@lmn.ro>
2015-06-01 02:12:19 -0700

    Iterative + recursive fibonacci

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// returns the maximum element from an array
+int max(int elements[], int size);
+// calculates the fibonacci sequence up for n using a recursive algorithm
+int fibonacci_recursive(int n);
+// calculates the fibonacci sequence up for n using an iterative algorithm
+int fibonacci_iterative(int n);
 
 int main(int argc, char* argv[])
 {
-	printf("Hello, world\n");
-	FILE* f = fopen("C:\\Windows\\System32\\drivers\\etc\\hosts", "r");
-	if (!f) {
-		printf("Error opening file\n");
-		return -1;
-	}
+	int input[] = { 5, -1, 4, 12 };
+	
+	// 2. function pointers
+	// direct invocation
+	printf("Max is %d\n", max(input, 4));
+	// function pointer invocation
+	int(*sortfunc)(int elements[], int size) = max;
+	printf("Max (function pointer) %d\n", sortfunc(input, 4));
+
+	// 3. recursivity
+	printf("recursive fibonacci(12) = %d\n", fibonacci_recursive(12));
+	printf("iterative fibonacci(12) = %d\n", fibonacci_iterative(12));
 	return 0;
 }
 
+int max(int elements[], int size)
+{
+	if (size < 1) 
+	{
+		return NULL;
+	}
+
+	int max = elements[0];
+	for (int i = 1; i < size; i++)
+	{
+		if (elements[i] > max)
+		{
+			max = elements[i];
+		}
+	}
+
+	return max;
+}
+
+int fibonacci_recursive(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return n;
+	}
+
+	return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
+}
+
+int fibonacci_iterative(int n)
+{
+
+	int* accumulator = (int *) malloc( (n+1) * sizeof(int)) ;
+
+	for (int i = 0; i <= n; i++)
+	{
+		if (i == 0 || i == 1)
+		{
+			accumulator[i] = i;
+		}
+		else {
+			accumulator[i] = accumulator[i - 1] + accumulator[i - 2];
+		}
+	}
+
+	int result = accumulator[n];
+
+	free(accumulator);
+
+	return result;
+}
